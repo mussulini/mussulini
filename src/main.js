639810e1a87568fbe1d5e5d03cffdb13b711ec08
Mussulini Lopez <mussulini.l@gmail.com>
2019-09-05 18:39:26 +0800

    Particle.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
+import VueParticles from 'vue-particles'
 
+import './registerServiceWorker'
 import "./assets/scss/app.scss";
 
 Vue.config.productionTip = false
+
+Vue.use(VueParticles)
 
 new Vue({
   router,
